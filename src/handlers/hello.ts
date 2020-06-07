0914aaefa60e26e32d1334fad3edc81da32c895d
Kenneth Freitas <kennethmf@outlook.com>
2020-06-07 17:15:14 -0300

    template base

---
+++ src/handlers/hello.ts
@@ -0,0 +1,9 @@
+import { APIGatewayProxyHandler } from "aws-lambda";
+import "source-map-support/register";
+
+export const hello: APIGatewayProxyHandler = async (_event, _context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ nice: true }),
+  };
+};
